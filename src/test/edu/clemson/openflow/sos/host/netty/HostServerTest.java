d215e28fc7ac250ce08e1364e0d5ec142980060f
Geddings Barrineau <cbarrin@g.clemson.edu>
2017-12-19 14:51:20 -0500

    Added easy mock and a couple of tests

--- src/test/edu/clemson/openflow/sos/host/netty/HostServerTest.java
+++ src/test/edu/clemson/openflow/sos/host/netty/HostServerTest.java
@@ -1,11 +1,31 @@
 package edu.clemson.openflow.sos.host.netty;
 
+import io.netty.channel.ChannelHandlerContext;
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Created by @geddings on 12/19/17.
  */
 class HostServerTest {
-    @org.junit.jupiter.api.BeforeEach
+    private HostServer hostServer;
+
+    @BeforeEach
     void setUp() {
+        hostServer = new HostServer();
+    }
+
+    @Test
+    void testHostServerStartsSocket() {
+        assertTrue(hostServer.start());
+    }
+    
+    @Test
+    void testHostServerChannelRead() {
+        hostServer.channelRead(EasyMock.createNiceMock(ChannelHandlerContext.class), "Message");
     }
 
 }
