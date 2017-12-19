989ec85946f47493b99c9164167630b984e29cf1
Geddings Barrineau <cbarrin@g.clemson.edu>
2017-12-19 14:02:05 -0500

    Adding JUnit 5 and test

---
+++ src/test/edu/clemson/openflow/sos/host/netty/HostServerTest.java
@@ -0,0 +1,11 @@
+package edu.clemson.openflow.sos.host.netty;
+
+/**
+ * Created by @geddings on 12/19/17.
+ */
+class HostServerTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+}
