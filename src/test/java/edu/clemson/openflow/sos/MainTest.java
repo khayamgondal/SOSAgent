c7659c2453fd134519ec44786cc1d4f96bd1e6e3
Khayam Gondal <khayam.gondal@gmail.com>
2018-02-21 23:19:22 -0500

    Tests setup completed

--- src/test/java/edu/clemson/openflow/sos/MainTest.java
+++ src/test/java/edu/clemson/openflow/sos/MainTest.java
@@ -3,14 +3,14 @@ package edu.clemson.openflow.sos;
 import edu.clemson.openflow.sos.agent.AgentServer;
 import edu.clemson.openflow.sos.host.HostServer;
 import edu.clemson.openflow.sos.rest.RestServer;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+public class MainTest {
+
     @Test
-    void main() {
+    public void main() {
         RestServer restServer = new RestServer();
         assertEquals(restServer.startComponent(), true);
 
