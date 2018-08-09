48ddcb41471be2f785a22c2380901e2555390a03
Khayam Gondal <khayam.gondal@gmail.com>
2018-08-09 17:24:27 -0400

    updated script not setting MTU or TC yet

--- setup/hw-setup.py
+++ setup/hw-setup.py
@@ -4,30 +4,37 @@
 import sys, os, string, threading
 import paramiko
 
-
 def host_conn(host):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, username=user, key_filename=key)
     update_nodes(host, ssh)
     install_ovs(host, ssh)
+    set_eth_ip(host, ssh)
 
 def agent_conn(host):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, username=user, key_filename=key)
     update_nodes(host, ssh)
-    install_java_8(host, ssh)
-    install_ovs(host, ssh)
+    #install_java_8(host, ssh)
+    #install_ovs(host, ssh)
+    #setup_sos(host, ssh)
     #install_infini_drivers(host, ssh)
+    set_infini_ip(host, ssh)
+    install_ovs(host, ssh)
+    set_eth_ip(host, ssh)
+    set_infini_route(host, ssh)
 
 def wan_conn(host):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, username=user, key_filename=key)
     update_nodes(host, ssh)
-    install_java_8(host, ssh)
-    setup_controller(host, ssh)
+    #install_infini_drivers(host, ssh)
+    set_infini_ip(host, ssh)
+    #install_java_8(host, ssh)
+    #setup_controller(host, ssh)
 
 def update_nodes(host, ssh):
     stdin, stdout, stderr = ssh.exec_command('sudo apt update')
@@ -53,6 +60,32 @@ def install_infini_drivers(host, ssh):
     else:
         print(host + " Failed to install drivers")
 
+def set_infini_ip(host, ssh):
+    stdin, stdout, stderr = ssh.exec_command('hostname')
+    exit_status = stdout.channel.recv_exit_status()
+    out_lines = stdout.readline()
+    hostname = out_lines.split('.')[0]
+    stdin, stdout, stderr = ssh.exec_command('sudo ifconfig ib0 ' + infini_ip_map.get(hostname) + '/24 up')
+    exit_status = stdout.channel.recv_exit_status()
+    if exit_status != 0:
+        print(host + " Failed to get hostname or set IP ")
+
+def set_eth_ip(host, ssh):
+    stdin, stdout, stderr = ssh.exec_command('hostname')
+    exit_status = stdout.channel.recv_exit_status()
+    out_lines = stdout.readline()
+    hostname = out_lines.split('.')[0]
+    stdin, stdout, stderr = ssh.exec_command('sudo ifconfig br0 ' + eth_ip_map.get(hostname) + '/24 up')
+    exit_status = stdout.channel.recv_exit_status()
+    if exit_status != 0:
+        print(host + " Failed to get hostname or set Eth IP on " + hostname)
+
+
+def set_infini_route(host, ssh):
+    stdin, stdout, stderr = ssh.exec_command('sudo route add -net 172.0.0.0/24 gw 172.0.0.100')
+    exit_status = stdout.channel.recv_exit_status()
+    if exit_status != 0:
+        print(host + " Failed to set route")
 
 def install_java_8(host, ssh):
     stdin, stdout, stderr = ssh.exec_command('sudo apt install -y openjdk-8-jdk')
@@ -73,18 +106,14 @@ def install_ovs(host, ssh):
         print(host + " Failed to install OVS")
 
 def setup_ovs(host, ssh):
-    stdin, stdout, stderr = ssh.exec_command('sudo ovs-vsctl add-br br0 && sudo ovs-vsctl add-port br0 '+ interface_name +' && sudo ifconfig '+ interface_name + ' 0 up && sudo ovs-vsctl add-port br0 '+ interface_name2 +' && sudo ifconfig '+ interface_name2 + ' 0 up')
+    stdin, stdout, stderr = ssh.exec_command('sudo ovs-vsctl add-br br0 && sudo ovs-vsctl add-port br0 '+ interface_name +' && sudo ifconfig '+ interface_name + ' 0 up ')
     exit_status = stdout.channel.recv_exit_status()
-    stdin, stdout, stderr = ssh.exec_command('sudo ovs-vsctl set-controller br0 tcp:'+ ctl_ip + ':'+ctl_port )
+    #stdin, stdout, stderr = ssh.exec_command('sudo ovs-vsctl set-controller br0 tcp:'+ ctl_ip + ':'+ctl_port )
     exit_status = stdout.channel.recv_exit_status()
     if exit_status == 0:
-        #print(host + " OVS setup!")
         pass
     else:
         print(host + " Failed to setup OVS")
-
-
-
 
 def setup_controller(host, ssh):
     install_maven(host, ssh)
@@ -94,6 +123,15 @@ def setup_controller(host, ssh):
         pass
     else:
         print(host + " Failed to setup floodlight controller")
+
+def setup_sos(host, ssh):
+    install_maven(host, ssh)
+    stdin, stdout, stderr = ssh.exec_command('git clone http://github.com/khayamgondal/SOSAgent && cd SOSAgent/ && git checkout dev && mvn package -Dmaven.test.skip=true')
+    exit_status = stdout.channel.recv_exit_status()
+    if exit_status == 0:
+        pass
+    else:
+        print(host + " Failed to setup sos")
 
 def install_maven(host, ssh):
     stdin, stdout, stderr = ssh.exec_command('sudo apt install -y maven')
@@ -105,12 +143,22 @@ def install_maven(host, ssh):
 
 
 
-clients = []
-agents = ['apt177.apt.emulab.net', 'apt057.apt.emulab.net', 'apt007.apt.emulab.net', 'apt060.apt.emulab.net', 'apt053.apt.emulab.net',
-          'apt184.apt.emulab.net']
-servers = []
+clients = ['apt128.apt.emulab.net', 'apt122.apt.emulab.net', 'apt075.apt.emulab.net', 'apt126.apt.emulab.net', 'apt079.apt.emulab.net']
 
-wan = 'apt184.apt.emulab.net'
+agents = ['apt082.apt.emulab.net', 'apt083.apt.emulab.net', 'apt125.apt.emulab.net', 'apt126.apt.emulab.net', 'apt085.apt.emulab.net',
+          'apt121.apt.emulab.net', 'apt078.apt.emulab.net', 'apt076.apt.emulab.net', 'apt086.apt.emulab.net', 'apt074.apt.emulab.net',]
+
+infini_ip_map = {'agent1':'172.0.0.11', 'agent2':'172.0.0.12', 'agent3':'172.0.0.13', 'agent4':'172.0.0.14', 'agent5':'172.0.0.15', 'agent6':'172.0.0.16',
+                 'agent7':'172.0.0.17', 'agent8':'172.0.0.18', 'agent9':'172.0.0.19', 'agent10':'172.0.0.10','wan':'172.0.0.100' }
+
+eth_ip_map = {'agent1':'10.0.0.11', 'agent2':'10.0.0.12', 'agent3':'10.0.0.13', 'agent4':'10.0.0.14', 'agent5':'10.0.0.15',
+              'agent6':'10.0.0.16',  'agent7':'10.0.0.17', 'agent8':'10.0.0.18', 'agent9':'10.0.0.19', 'agent10':'10.0.0.10','wan':'10.0.0.100',
+              'client1':'10.0.0.111', 'client2':'10.0.0.112', 'client3':'10.0.0.113', 'client4':'10.0.0.114', 'client5':'10.0.0.115',
+              'server1':'10.0.0.211', 'server2':'10.0.0.212', 'server3':'10.0.0.213', 'server4':'10.0.0.214', 'server5':'10.0.0.215'}
+
+servers = ['apt127.apt.emulab.net', 'apt081.apt.emulab.net', 'apt123.apt.emulab.net', 'apt080.apt.emulab.net', 'apt084.apt.emulab.net']
+
+wan = 'apt077.apt.emulab.net'
 
 user = 'khayam'
 key = '/home/khayam/.ssh/id_rsa'
