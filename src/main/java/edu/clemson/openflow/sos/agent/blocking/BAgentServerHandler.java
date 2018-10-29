2b1dcc2ab3ac082333bdc8cf010b0e034dc78515
Khayam Gondal <khayam.gondal@gmail.com>
2018-10-29 12:18:02 -0400

    still working

--- src/main/java/edu/clemson/openflow/sos/agent/blocking/BAgentServerHandler.java
+++ src/main/java/edu/clemson/openflow/sos/agent/blocking/BAgentServerHandler.java
@@ -1,20 +1,23 @@
 package edu.clemson.openflow.sos.agent.blocking;
 
+import edu.clemson.openflow.sos.host.blocking.BHostClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.crypto.Data;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class BAgentServerHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(BAgentServerHandler.class);
     private Socket socket;
 
+    private BHostClient bHostClient = new BHostClient("10.0.0.211", 5001);
+    private Socket hostClientSocket;
+
     public BAgentServerHandler(Socket s) {
         socket = s;
+        hostClientSocket = bHostClient.connectSocket();
     }
 
     @Override
@@ -22,9 +25,15 @@ public class BAgentServerHandler extends Thread {
         log.info("connected to {}", socket.getInetAddress().getHostAddress());
         DataInputStream dis = null;
         DataOutputStream dos = null;
+        BufferedReader hdis = null;
+        DataOutputStream hdos = null;
+
         try {
             dis = new DataInputStream(socket.getInputStream());
             dos = new DataOutputStream(socket.getOutputStream());
+
+            hdis = new BufferedReader( new InputStreamReader(hostClientSocket.getInputStream()));
+            hdos = new DataOutputStream(hostClientSocket.getOutputStream());
 
         } catch (IOException e) {
             e.printStackTrace();
